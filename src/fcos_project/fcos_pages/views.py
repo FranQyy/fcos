74199a3dfd70eda806c5f3fa2c8716d3442dffc7
Lukasz Czerniak <lfczerniak@gmail.com>
2018-09-16 11:18:56 +0200

    adds Ipstack

--- src/fcos_project/fcos_pages/views.py
+++ src/fcos_project/fcos_pages/views.py
@@ -3,10 +3,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-from .forms import LoginForm
-from django.views.generic.edit import UpdateView
-
+from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
+
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .forms import CustomUserCreationForm
+
+import requests
 
 # Create your views here.
 
@@ -15,7 +20,15 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def localisations(request):
-  return render(request, 'fcos_pages/content/localisations.html')
+  ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+  print(ip_address)
+  response = requests.get('http://api.ipstack.com/check?access_key=24eb765a336f5b614aee079d61416188')
+  geodata = response.json()
+  return render(request, 'fcos_pages/content/localisations.html', {
+        'ip': geodata['ip'],
+        'country': geodata['country_name'],
+        'city': geodata['city'],
+  })
 
 @login_required
 def activities(request):
@@ -25,30 +38,25 @@ def activities(request):
 def profile(request):
   return render(request, 'fcos_pages/content/profile.html')
 
-def user_login(request):
-  if request.method == 'POST':
-    form = LoginForm(request.POST)
-    if form.is_valid():
-      data = form.cleaned_data
-      email = data['email']
-      password = data['password']
-      user = authenticate(email=email, password=password)
-      if user is not None:
-        if user.is_active:
-          login(request, user)
-          return render(request, 'fcos_pages/content/index.html')
-        else:
-          return HttpResponse('Konto jest zablokowane.')
-      else:
-        return HttpResponse('Nieprawidłowe dane uwierzytelniające.')
-  else:
-    form = LoginForm()
-  return render(request, 'fcos_pages/content/user_login.html', {'form': form})
-
-from django.urls import reverse_lazy
-from django.views import generic
-
-from .forms import CustomUserCreationForm
+# def user_login(request):
+#   if request.method == 'POST':
+#     form = LoginForm(request.POST)
+#     if form.is_valid():
+#       data = form.cleaned_data
+#       email = data['email']
+#       password = data['password']
+#       user = authenticate(email=email, password=password)
+#       if user is not None:
+#         if user.is_active:
+#           login(request, user)
+#           return render(request, 'fcos_pages/content/index.html')
+#         else:
+#           return HttpResponse('Konto jest zablokowane.')
+#       else:
+#         return HttpResponse('Nieprawidłowe dane uwierzytelniające.')
+#   else:
+#     form = LoginForm()
+#   return render(request, 'fcos_pages/content/user_login.html', {'form': form})
 
 class SignUp(generic.CreateView):
   form_class = CustomUserCreationForm
